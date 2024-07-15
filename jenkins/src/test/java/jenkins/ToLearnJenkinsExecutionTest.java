9faa27af93de3f7a92c826107ae2f00d2ea57844
Sathish kumar Ravi <Sathish kumar Ravi@DESKTOP-VLSES14>
2024-07-15 09:47:54 +0530

    first commit

---
+++ jenkins/src/test/java/jenkins/ToLearnJenkinsExecutionTest.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnJenkinsExecutionTest 
+{
+@Test
+public void execute()
+{
+	Reporter.log("Jenkins class executed", true);
+}
+}
