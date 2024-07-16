98624a6cf9fb3bc6a4565f09ac017882cee69649
Sathish kumar Ravi <Sathish kumar Ravi@DESKTOP-VLSES14>
2024-07-16 10:06:30 +0530

    CHANGES TUESDAY

---
+++ jenkins/src/test/java/jenkins/ToLearnPollSCMTest.java
@@ -0,0 +1,14 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ToLearnPollSCMTest
+{
+	@Test
+	public void launch()
+	{
+		Reporter.log("POLL SCM SUCCESS");
+	}
+
+}
